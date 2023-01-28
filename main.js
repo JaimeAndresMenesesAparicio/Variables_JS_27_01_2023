95f1f8fde5cfb50b6983979dba597a56358dd2c4
JaimeAndresMenesesAparicio <jaime.meneses89@gmail.com>
2023-01-28 15:14:33 -0500

    Variables JS

---
+++ main.js
@@ -0,0 +1,12 @@
+let materia = `Programación`;
+let promedio = 4.8;
+let aprobo = true;
+let carnet = {Nombre: "Jaime", Carrera: "Electrónica", Universidad: "UTS",};
+
+console.log(`La materia es ${materia}`);
+console.log(`El promedio es ${promedio}`);
+console.log(`Aprobo ${aprobo}`);
+console.log(`CARNET`);
+console.log(`${carnet.Nombre}`);
+console.log(`${carnet.Carrera}`);
+console.log(`${carnet.Universidad}`);
